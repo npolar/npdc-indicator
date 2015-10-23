bde3e9d78f1d979878e9b6bc88b7b97325b98d3e
cnrdh <cnrdh@users.noreply.github.com>
2015-10-23 13:07:26 +0200

    const for schema

--- src/timeseries/TimeseriesEditController.js
+++ src/timeseries/TimeseriesEditController.js
@@ -3,11 +3,14 @@
 // @ngInject
 var TimeseriesEditController = function($scope, $controller, $routeParams, Timeseries, NpolarTranslate) {
   
+  const schema = '//api.npolar.no/schema/indicator-timeseries-1';
+  //const schema = 'schema/indicator-timeseries-1.json';
+  
   $controller("NpolarEditController", {$scope: $scope});
   $scope.resource = Timeseries;
   $scope.T = NpolarTranslate;
   
-  $scope.formula.schema = '//api.npolar.no/schema/indicator-timeseries-1';
+  $scope.formula.schema = schema;
   $scope.formula.form = "timeseries/timeseries-formula.json";
   $scope.formula.template = 'material';
   
