6e23332b4396477a69a7e1768d62799bc627b6b1
cnrdh <cnrdh@users.noreply.github.com>
2017-01-27 13:11:14 +0100

    Updated common

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
 var npdcGulp = require('npdc-gulp');
 var config = npdcGulp.baseConfig;
-config.COMMON_VERSION = '4.8.3';
+config.COMMON_VERSION = '4.8.4';
 npdcGulp.loadAppTasks(gulp, config);
